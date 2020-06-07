c148cde7c433177bd372629b630ffa9ef8d4efe4
Marcitox <benitez4721@gmail.com>
2020-06-07 03:24:23 -0400

    Agreganod variable de entonrno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB
 if ( process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://benitez4721:4721566@cluster0-b7fhw.mongodb.net/cafe'
+    urlDB = process.env.urlProd
 }
 
 process.env.URLDB = urlDB
